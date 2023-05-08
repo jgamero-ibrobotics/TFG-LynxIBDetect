6a5a67517cfdc10b1e8d26ef5070eba1d02c6a1c
jgamero-ibrobotics <jesgambor@alum.us.es>
2023-05-08 19:56:57 +0200

    IoU2

--- IoU.py
+++ IoU.py
@@ -9,16 +9,15 @@ import xml.etree.ElementTree as ET
 def read_xml_file(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-
-    boxes = []
+    references = []
     for obj in root.findall('object'):
         xmin = int(obj.find('bndbox/xmin').text)
         ymin = int(obj.find('bndbox/ymin').text)
         xmax = int(obj.find('bndbox/xmax').text)
         ymax = int(obj.find('bndbox/ymax').text)
-        boxes.append((xmin, ymin, xmax, ymax))
-
-    return boxes
+        references.append([obj.find('name').text,xmin, ymin, xmax, ymax])
+ 
+    return references
 
 def apply_detection_model(image_path):
     # Cargar la imagen con OpenCV
@@ -54,7 +53,6 @@ def apply_detection_model(image_path):
             object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
 
             detections.append([object_name, scores[i], xmin, ymin, xmax, ymax]) # Add detection data to list
-            
 
     return detections
 
@@ -154,7 +152,7 @@ else: # This is a TF1 model
 
 
 # Configuración de Dropbox
-access_token = 'sl.Bd9_cv2EcxEA5yzRZtPkS50lQKKFqUtoWBpQ7MTjicotTb_YdYX4c36fqrpXiVwQ1Ug6Rg6Q7yFxNYelqVRk2UXHvc_tAJCdu8xUxBtQlauGzGd4TA3IoMG5I-qjdEZh9wq2gMZQr_w'
+access_token = 'sl.BeClovDMT5Mdyl_mocTAZPKmo_BNUsf49LtVXRBpvxtpcd86noWtV7i7uMJ6hziKMgufZTWp8nAC6nOfOXkmUASxjlmJ4UXRHZFXxlJ9jdAkdBnl1iakGbFE8WUkxayesuKUB0PpDAc'
 dropbox_folder = '/test'
 
 # Directorio local para descargar los archivos
@@ -176,7 +174,12 @@ for nombre_archivo in os.listdir("/home/pi/TFG-LynxIBDetect/IoU"):
     if os.path.isfile(ruta_archivo):
         os.remove(ruta_archivo)
 
-j=0
+j = 0
+k = 0
+iou_total = 0
+Tp = 0
+Fp = 0
+Fn = 0
 for file_entry in file_list:
     # Obtener el nombre del archivo
     file_name = file_entry.name
@@ -188,9 +191,8 @@ for file_entry in file_list:
         client.files_download_to_file(xml_path, f'{dropbox_folder}/{file_name}')
 
         # Leer las coordenadas de boxA desde el archivo XML
-        boxA = read_xml_file(xml_path)
-        print('boxA')
-        print(boxA)
+        references = read_xml_file(xml_path)
+  
         # Obtener el nombre del archivo de imagen relacionado
         image_file = file_name.replace('.xml', '.jpg')
 
@@ -205,25 +207,56 @@ for file_entry in file_list:
         os.remove(image_path)
         os.remove(xml_path)
 
-        iou_scores = []
-        for box in boxA:
+        iou_scores = [] 
+        for reference in references: # recorre cada box en el archivo xml
+            boxA = [reference[1],  reference[2],  reference[3],  reference[4]] # [xmin, ymin, xmax, ymax]
+            clase_ref = reference[0]
+            print('box Referencia')
+            print(boxA)
             iou_scores_box = []
-            for detection in detections:
+            cv2.rectangle(image, (boxA[0],  boxA[1]), (boxA[2],  boxA[3]), (0, 255, 0), 2) # ground truth box
+            if detections == []: # si no hay detecciones
+                for i in range(len(boxA)):
+                    detections.append(["NONE", 0, 0, 0, 0, 0])
+            for detection in detections: # recorre cada box por objeto detectado
                 boxB = [detection[2],  detection[3],  detection[4],  detection[5]] # [xmin, ymin, xmax, ymax]
+                clase_det = detection[0]
                 cv2.rectangle(image, (boxB[0],  boxB[1]), (boxB[2],  boxB[3]), (0, 0, 255), 2) # detection box
-                cv2.rectangle(image, (box[0],  box[1]), (box[2],  box[3]), (0, 255, 0), 2) # ground truth box
-                print('boxB')
+                print('box Detección')
                 print(boxB)
-                iou_score = calculate_iou(box, boxB)
+
+                # Calcular el coeficiente de IoU
+                iou_score = calculate_iou(boxA, boxB)
                 iou_scores_box.append(iou_score)
             max_iou_score = max(iou_scores_box)
+
+            if max_iou_score > 0.7 and clase_ref == clase_det:
+                Tp += Tp
+            elif clase_ref == clase_det and max_iou_score < 0.7:
+                Fp += Fp
+            elif max_iou_score > 0.7 and clase_ref != clase_det:
+                Fn += Fn 
+
             iou_scores.append(max_iou_score)
+            k = k + 1
 
         file_name = f'/home/pi/TFG-LynxIBDetect/IoU/image_IoU_{j+1}.jpg'
         j=j+1
-        cv2.imwrite(file_name,image)  # Guardar imagen en un archivo
-        iou_total = sum(iou_scores)
+        #cv2.imwrite(file_name,image)  # Guardar imagen en un archivo
+        iou_total = sum(iou_scores) + iou_total
 
-        print(f"Archivo XML: {file_name}")
+        #print(f"Archivo XML: {file_name}")
         print("El coeficiente de IoU es:", iou_scores)
-        print("-----------------------------------------")
+        print("---------------------------------------------")
+
+print("   ")
+print("--------------RESULTADOS FINALES--------------")
+precision = Tp/(Tp+Fp)
+recall = Tp/(Tp+Fn)
+f1_score = 2*((precision*recall)/(precision+recall))
+print("Verdaderos Positivos:", Tp)
+print("Falsos Positivos:", Fp)
+print("Falsos Negativos:", Fn)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1_score)
